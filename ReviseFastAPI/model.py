dd5ba080fda82db1371e161d1fe039d220f2cc58
Prashant Jha <prashantjha9549@gmail.com>
2026-02-16 16:21:39 +0530

    master:- learniing fastAPI from scratch

--- ReviseFastAPI/model.py
+++
@@ -1,3 +0,0 @@
-from pydantic import BaseModel
-class Pydantic_Model(BaseModel):
-    name:str
--- fastAPI/database.py
+++
@@ -1,5 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-db_url = 'postgresql://postgres:password@localhost:5432/jhaStore'
-engine = create_engine(db_url)
-session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
