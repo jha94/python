11f2309eada0d699ba6dfb6f646180be699e0af8
Prashant Jha <prashantjha9549@gmail.com>
2026-04-06 12:40:41 +0530

    fastAPI :- start from scratch

--- fastAPI/database.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-db_url = 'postgresql://postgres:password@localhost:5432/first'
-engine = create_engine(db_url)
-session = sessionmaker(autoCommit=False, autoFlush=False, bind=engine)
--- fastAPI/models.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-class Product(BaseModel):
-    id:int
-    name:str
-    desc:str
